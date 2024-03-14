ba549fb5138053e31ee225c7baf00b6705c2b0c3
Shivam Patel <shivampatek0048@gmail.com>
2024-03-15 00:18:14 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Bussiness from "./Bussiness";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Bussiness,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+};
